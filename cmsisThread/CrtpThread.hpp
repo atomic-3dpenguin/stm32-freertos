888436e82cda0cf4e728cb3930eb466fdbadc559
Atomic Penguin <atomic.3dpenguin@gmail.com>
2025-05-18 16:00:24 -0700

    Added CMSIS Thread Design pattern.

---
+++ cmsisThread/CrtpThread.hpp
@@ -0,0 +1,57 @@
+/*
+ * CrtpThread.hpp
+ *
+ *  Created on: May 18, 2025
+ *      Author: cavem
+ */
+
+#ifndef CRTPTHREAD_HPP_
+#define CRTPTHREAD_HPP_
+
+
+#include "cmsis_os2.h"
+
+template<typename Derived>
+class CrtpThread {
+public:
+  CrtpThread(osPriority_t priority = osPriorityNormal,
+             uint32_t    stack_size = 512,
+             const char* name       = nullptr)
+  {
+    // Create an event-flags object
+    evt_id_ = osEventFlagsNew(nullptr);
+
+    // Set up thread attributes
+    osThreadAttr_t attr{};
+    attr.name       = name;
+    attr.priority   = priority;
+    attr.stack_size = stack_size;
+
+    // Spawn the thread, passing 'this' as argument
+    thread_id_ = osThreadNew(&CrtpThread::threadEntry, this, &attr);
+  }
+
+  /// Send an event flag; picks the ISR-safe API if inside an interrupt.
+  void sendEvent(uint32_t flags) {
+
+      osEventFlagsSet(evt_id_, flags);
+  }
+
+  /// Wait until any of the specified flags are set (or timeout)
+  uint32_t waitEvent(uint32_t flags, uint32_t timeout = osWaitForever) {
+    // osFlagsWaitAny will clear the bits by default; adjust if you need different behavior
+    return osEventFlagsWait(evt_id_, flags, osFlagsWaitAny, timeout);
+  }
+
+protected:
+
+  /// Cast back to the derived class and call its run()
+  static void threadEntry(void* arg) {
+    static_cast<Derived*>(arg)->run();
+  }
+
+  osThreadId_t     thread_id_;
+  osEventFlagsId_t evt_id_;
+};
+
+#endif /* CRTPTHREAD_HPP_ */
---
+++ cmsisThread/myThread.hpp
@@ -0,0 +1,51 @@
+/*
+ * myThread.hpp
+ *
+ *  Created on: May 18, 2025
+ *      Author: cavem
+ */
+
+#ifndef MYTHREAD_HPP_
+#define MYTHREAD_HPP_
+
+#include "CrtpThread.hpp"
+#include "cmsis_os2.h"
+#include <cstdio>
+
+class MyThread : public CrtpThread<MyThread> {
+public:
+  // Automatically starts with high priority, 1 KiB stack, named "MyThr"
+  MyThread()
+    : CrtpThread(osPriorityHigh, 1024, "MyThr")
+  {}
+
+  // This is the entry point for the new thread
+  void run() {
+    constexpr uint32_t FLAG_WORK = (1u << 0);
+
+    for (;;) {
+      // Wait for FLAG_WORK from either thread or ISR
+      uint32_t ev = waitEvent(FLAG_WORK);
+
+      if (ev & FLAG_WORK) {
+        // ... do your work here ...
+    	  printf("Working\r\n");
+      }
+    }
+  }
+
+  // Can be called from thread context
+  void triggerWork() {
+    sendEvent(1u << 0);
+  }
+
+  // Can be called from an ISR too
+  void triggerWorkFromISR() {
+    sendEvent(1u << 0);
+  }
+};
+
+
+
+
+#endif /* MYTHREAD_HPP_ */
